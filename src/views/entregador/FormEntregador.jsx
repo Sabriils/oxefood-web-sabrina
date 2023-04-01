a39b881a3f3bcbaf448e06bf5cab3e36aebcfbd9
sabrina <sabribelly25@gmail.com>
2023-04-01 17:36:53 -0300

    alterações

--- src/views/entregador/FormEntregador.jsx
+++ src/views/entregador/FormEntregador.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 import InputMask from 'react-input-mask';
-import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
+import { Button, Container, Divider, Form, Icon, Select, Radio} from 'semantic-ui-react';
+
+const options = [
+	{ key: "AC", text: "Acre", value: "AC" },
+  { key: "AL", text: "Alagoas", value: "AL" },
+  { key: "AP", text: "Amapá", value: "AP" },
+  { key: "AM", text: "Amazonas", value: "AM" },
+  { key: "BA", text: "Bahia", value: "BA" },
+  { key: "CE", text: "Ceará", value: "CE" },
+  { key: "DF", text: "Distrito Federal", value: "DF" },
+  { key: "ES", text: "Espírito Santo", value: "ES" },
+  { key: "GO", text: "Goiás", value: "GO" },
+  { key: "MA", text: "Maranhão", value: "MA" },
+  { key: "MT", text: "Mato Grosso", value: "MT" },
+  { key: "MS", text: "Mato Grosso do Sul", value: "MS" },
+  { key: "MG", text: "Minas Gerais", value: "MG" },
+  { key: "PA", text: "Pará", value: "PA" },
+  { key: "PB", text: "Paraíba", value: "PB" },
+  { key: "PR", text: "Paraná", value: "PR" },
+  { key: "PE", text: "Pernambuco", value: "PE" },
+  { key: "PI", text: "Piauí", value: "PI" },
+  { key: "RJ", text: "Rio de Janeiro", value: "RJ" },
+  { key: "RN", text: "Rio Grande do Norte", value: "RN" },
+  { key: "RS", text: "Rio Grande do Sul", value: "RS" },
+  { key: "RO", text: "Rondônia", value: "RO" },
+  { key: "RR", text: "Roraima", value: "RR" },
+  { key: "SC", text: "Santa Catarina", value: "SC" },
+  { key: "SP", text: "São Paulo", value: "SP" },
+  { key: "SE", text: "Sergipe", value: "SE" },
+  { key: "TO", text: "Tocantins", value: "TO" },
+];
+
 
 class FormEntregador extends React.Component{
+	state = {}
 
+	handleChange = (e, { value }) => this.setState({ value })
     render(){
+		const { value } = this.state
         return(
             <div>
 
@@ -138,6 +172,48 @@ class FormEntregador extends React.Component{
 										mask="99.999-999"/> 
 									</Form.Input>
 
+								</Form.Group>
+
+								<Form.Group>
+									<Form.Input
+										fluid
+										label='UF'
+                                        width={16}>
+									</Form.Input>
+								</Form.Group>
+
+								<Form.Group>
+										<Form.Field
+										control={Select}
+										label='UF'
+										width={16}
+										options={options}
+										placeholder='selecione'
+									/>
+								</Form.Group>
+								<Form.Group>
+									<Form.Input
+										fluid
+										label='Complemento'
+                                        width={16}>
+									</Form.Input>
+								</Form.Group>
+								<Form.Group inline>
+									<label>Ativo: </label>
+										<Form.Radio
+											control={Radio}
+											label='Sim'
+											value='si'
+											checked={value === 'si'}
+											onChange={this.handleChange}
+										/>
+										<Form.Radio
+											control={Radio}
+											label='Não'
+											value='no'
+											checked={value === 'no'}
+											onChange={this.handleChange}
+										/>
 								</Form.Group>
 
 								<Form.Group widths='equal' style={{marginTop: '4%'}}  className='form--empresa-salvar'>
